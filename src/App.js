4057fbe79dcc148934c352cb0ac791fadf5fa401
abhimehra4967 <abhishekmehra1402@gmail.com>
2020-11-30 12:51:39 +0530

    rouitng added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 
 import './App.css';
-import Homepage from "./homepage.component";
+import Homepage from "./components/homepage/homepage.component";
+import {Switch,Link, Route, Router, withRouter} from "react-router-dom";
 
 
+
+const SecondPage=()=>(
+<div>
+  hello
+</div>
+)
 function App() {
   return (
-    <div className="App">
-        <Homepage/>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Homepage}/> 
+        <Route exact path="/shop/hats" component={SecondPage}/>
         
+      </Switch>
     </div>
+    
   );
 }
 
